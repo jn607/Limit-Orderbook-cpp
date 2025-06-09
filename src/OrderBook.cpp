0135a11cbb32995823cdde2a4b7ce629d3d8d38d
Josh Nolan <joshynolan02@gmail.com>
2025-06-09 18:02:30 +0100

    format

---
+++ src/OrderBook.cpp
@@ -0,0 +1,14 @@
+#include "OrderBook.h"
+#include <iostream>
+
+// Define member function so linker knows how to implement it.
+void OrderBook::add_order(const Order& o) {
+    // for now, just insert every order into the correct side
+    if (o.side == Side::Buy) {
+        bids[o.price].push_back(o);
+        std::cout << "Added Buy@" << o.price << " q=" << o.quantity << "\n";
+    } else {
+        asks[o.price].push_back(o);
+        std::cout << "Added SELL@" << o.price << " q=" << "\n";
+    }
+}
